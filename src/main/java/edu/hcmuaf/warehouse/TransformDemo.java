403f76175306b29155a939ec21247c871000ff67
JeremieBelpois <Jeremiebelpois97@gmail.com>
2018-09-13 16:13:17 +0700

    Chay file ktr Da test va chay thu file ktr bang trans

--- src/main/java/edu/hcmuaf/warehouse/TransformDemo.java
+++ src/main/java/edu/hcmuaf/warehouse/TransformDemo.java
@@ -10,7 +10,10 @@ import org.pentaho.di.trans.TransMeta;
 public class TransformDemo {
     public static void main(String[] args) {
         try {
+            //khoi tao moi truong
             KettleEnvironment.init();
+
+            // ket noi toi co so du lieu nguon
             DatabaseMeta source1 = new DatabaseMeta();
             source1.setDatabaseInterface(DatabaseMeta.getDatabaseInterface("MS SQL Server"));
             source1.setName("BHYT");
@@ -23,30 +26,40 @@ public class TransformDemo {
             database1.connect();
 
             DatabaseMeta souce2 = new DatabaseMeta();
-        souce2.setDatabaseInterface(DatabaseMeta.getDatabaseInterface("MS SQL Server"));
-        souce2.setName("QL_BN_BHYT");
-        souce2.setHostname("DESKTOP-RKF9I56");
-        souce2.setDBName("QL_BN_BHYT");
-        souce2.setDBPort("1433");
-        souce2.setUsername("sa");
-        souce2.setPassword("a");
-        Database database2 = new Database(null, souce2);
-        database2.connect();
+            souce2.setDatabaseInterface(DatabaseMeta.getDatabaseInterface("MS SQL Server"));
+            souce2.setName("QL_BN_BHYT");
+            souce2.setHostname("DESKTOP-RKF9I56");
+            souce2.setDBName("QL_BN_BHYT");
+            souce2.setDBPort("1433");
+            souce2.setUsername("sa");
+            souce2.setPassword("a");
+            Database database2 = new Database(null, souce2);
+            database2.connect();
 
-        DatabaseMeta destination = new DatabaseMeta();
-        destination.setDatabaseInterface(DatabaseMeta.getDatabaseInterface("MS SQL Server"));
-        destination.setName("DW_Lab5");
-        destination.setHostname("DESKTOP-RKF9I56");
-        destination.setDBName("lab5");
-        destination.setDBPort("1433");
-        destination.setUsername("sa");
-        destination.setPassword("a");
-        Database database = new Database(null, destination);
-        database.connect();
+            // ket noi toi co so du lieu dich
+            DatabaseMeta destination = new DatabaseMeta();
+            destination.setDatabaseInterface(DatabaseMeta.getDatabaseInterface("MS SQL Server"));
+            destination.setName("DW_Lab5");
+            destination.setHostname("DESKTOP-RKF9I56");
+            destination.setDBName("lab5");
+            destination.setDBPort("1433");
+            destination.setUsername("sa");
+            destination.setPassword("a");
+            Database database = new Database(null, destination);
+            database.connect();
 
-        TransMeta transMeta = new TransMeta("E:/User/Code/intellij/KettleApi/src/file/lab5.ktr");
-        Trans trans = new Trans(transMeta);
-        trans.execute(null);
+            // thuc hien transform tu file ktr
+            //TransMeta: xac dinh thong tin ve transformation cung cap cac method  de luu tru va load
+            TransMeta transMeta = new TransMeta("E:/User/Code/intellij/KettleApi/src/file/lab5.ktr");
+            //Lớp này đại diện cho thông tin và các hoạt động liên kết với khái niệm transformation, tải, instantiates, khởi tạo, chạy, và giám sát việc thực hiện chuyển đổi
+            Trans trans = new Trans(transMeta);
+            // thuc hien cac chuyen doi
+            trans.execute(null);
+            trans.waitUntilFinished();
+// ngat ket noi toi co so du lieu
+            database1.disconnect();
+            database2.disconnect();
+            database.disconnect();
         } catch (KettleException e) {
             e.printStackTrace();
         }
